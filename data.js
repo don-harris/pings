3247446fcfa1941a16be1dee954d4fbbf82ff27f
dazyang <dazyang.contact@gmail.com>
2017-10-24 10:30:44 +1300

    seeds and data.js created

---
+++ data.js
@@ -0,0 +1,127 @@
+module.exports = {
+  users: [
+    {
+      id: 1001,
+      name: 'Callan',
+      github: 'callan-stretton',
+      photo_url: '/images/cal.png',
+      username: 'Callan the Careless Whisper',
+      superpower: 'He will whisper sweet code into your ear and comatose you into a deep deep slumber',
+      backStory: 'From the isles of Miramar',
+      hash: ''
+    }, {
+      id: 1002,
+      name: 'Raj',
+      github: 'rajal-kumar',
+      photo_url: '/images/raj.jpg',
+      username: 'The Fijian Taxi Man',
+      superpower: 'Can teleport people to other places in milliseconds',
+      backStory: 'A mix of Indian and Fijian heritage, The Taxi man has aquired the skills to deal with his enemies with strength and hardwork',
+      hash: ''
+    }, {
+      id: 1003,
+      name: 'Joe',
+      github: 'JosephJvB',
+      photo_url: '/images/joe.jpg',
+      username: 'Joey Badass',
+      superpower: 'Spits hot fire as he raps.',
+      backStory: "Now this is the story all about how my life got flipped, turned upside down and I'd like to take a minute if I may I'll tell you how I became the prince of a town called EDA",
+      hash: ''
+    }, {
+      id: 1004,
+      name: 'Richard',
+      github: 'richardjohns',
+      photo_url: '/images/rich.jpg',
+      username: 'Long John Silver',
+      superpower: 'Creates long johns out of thin air which will suffocate his opponent',
+      backStory: 'Born on the backstreets in the biggest and toughest accounting city; The city of Compound Interest, located on planet dividends. Raised to be as brutal with accounts payable as with long johns, a hero to be respected and feared',
+      hash: ''
+    }, {
+      id: 1005,
+      name: 'Don',
+      github: 'don-smith',
+      photo_url: '/images/don.jpeg',
+      username: 'The Code Thief',
+      superpower: 'Can take away your superpower by hacking your DNA with code',
+      backStory: 'Once a star pupil of the EDA land, he turned Rogue and became the heroes deadliest code destroyer',
+      hash: ''
+    }, {
+      id: 1006,
+      name: 'Harrison',
+      github: 'harrison-symes',
+      photo_url: '/images/harry.jpeg',
+      username: 'Ladybug Killer',
+      superpower: 'Can turn into a lady bug but will go around stabbing people',
+      backStory: "sfdgsdgsdf help me I've been stabbed by a hairy ladybug! Ah well if you can't beat em join em...",
+      hash: ''
+    }, {
+      id: 1007,
+      name: 'Skye',
+      github: 'SkyeBelinda',
+      photo_url: '/images/skye.jpeg',
+      username: 'The Sloth of the Skies',
+      superpower: 'Relaxed but deadly, she can fly while sleeping',
+      backStory: "It's a bird!.. it's a plane!... it's! zzzzzzzzzzzzzzzzz...",
+      hash: ''
+    }, {
+      id: 1008,
+      name: 'Aaron',
+      github: 'aaron-macdonald',
+      photo_url: '/images/aaron.png',
+      username: 'The Coach',
+      superpower: 'Super at softball, the Coach does not play around. He will fire a thousand softballs at you before you can say THE SERVER IS DOWN',
+      backStory: 'Coaching all his life',
+      hash: ''
+    }, {
+      id: 1009,
+      name: 'Andrea',
+      github: 'amassignani',
+      photo_url: '/images/andrea.jpeg',
+      username: 'The Bombastic Brazilian',
+      superpower: 'Her joyous laugh makes her enemies spin and into a state of calm',
+      backStory: 'Born in the heart of Brazil, taught to fight crime and have a good time',
+      hash: ''
+    }, {
+      id: 1010,
+      name: 'Octavia',
+      github: 'palmeroctavia',
+      photo_url: '/images/oct.jpeg',
+      username: 'OctoLady',
+      superpower: 'OctoLady can multi-punch like no other being, she overwhelms her enemies with using too many attacks on them at once',
+      backStory: 'Talking to octopi all her life she has developed her skills to use her arms as 8 limbs rather than 2.',
+      hash: ''
+    }, {
+      id: 1011,
+      name: 'Daz',
+      github: 'dazyang',
+      photo_url: '/images/daz.jpeg',
+      username: 'Freeform',
+      superpower: 'Turns into water so she can drown her enemies.',
+      backStory: 'Oh hi Daz how was your weeke- agh!blugglugblahhhhcroooak',
+      hash: ''
+    }, {
+      id: 1012,
+      name: 'Annah',
+      github: 'AnnahGerletti',
+      photo_url: '/images/annah.jpeg',
+      username: 'Wonder Woman',
+      superpower: 'Clone myself',
+      backStory: "What's better than one Wonder Woman?? Well two of course...",
+      hash: ''
+    }, {
+      id: 1013,
+      name: 'Don H',
+      github: 'don-harris',
+      photo_url: '/images/donh.jpeg',
+      username: 'The Murricane',
+      superpower: 'Keeps living the same day over... and over... and over... and over... and plays a mean piano',
+      backStory: 'This fearsome warrior trained for thousands of years to master the little-known martial art of Murri-foo',
+      hash: ''
+    }
+  ],
+  pings: [
+    { id: 1, sender_id: 1001, recipient_id: 1005, image_url: '/images/lol.gif' },
+    { id: 2, sender_id: 1006, recipient_id: 1010, image_url: '/images/dog.gif' },
+    { id: 3, sender_id: 1003, recipient_id: 1008, image_url: '/images/meme.gif' }
+  ]
+}
--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -29,7 +29,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
