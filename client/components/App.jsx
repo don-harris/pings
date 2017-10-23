d174a49ad31dd3f2352c16585f4577b0c56f2935
student <>
2017-10-24 10:28:24 +1300

    routes are going peeps

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import ErrorMessage from './ErrorMessage'
 import WaitIndicator from './WaitIndicator'
@@ -6,6 +7,9 @@ import WaitIndicator from './WaitIndicator'
 const App = () => (
   <div className='app'>
     <ErrorMessage />
+    <Route exact path='/' component={() => <h1>Pings</h1>} />
+    <Route exact path='/new' component={() => <h1>New Ping</h1>} />
+    <Route exact path='/profile' component={() => <h1>Profile</h1>} />
     <WaitIndicator />
   </div>
 )
