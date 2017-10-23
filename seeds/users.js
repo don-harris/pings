41cf28a064bbaac0bdff9500da9af81c3c8fd230
dazyang <dazyang.contact@gmail.com>
2017-10-24 10:48:21 +1300

    edited usernames added userphotos in images folder

--- seeds/users.js
+++ seeds/users.js
@@ -6,91 +6,91 @@ exports.seed = function (knex, Promise) {
           id: 1001,
           name: 'Callan',
           photo_url: '/images/cal.png',
-          username: 'Callan the Careless Whisper',
+          username: 'callan',
           hash: ''
         }),
         knex('users').insert({
           id: 1002,
           name: 'Raj',
           photo_url: '/images/raj.jpg',
-          username: 'The Fijian Taxi Man',
+          username: 'raj',
           hash: ''
         }),
         knex('users').insert({
           id: 1003,
           name: 'Joe',
           photo_url: '/images/joe.jpg',
-          username: 'Joey Badass',
+          username: 'joe',
           hash: ''
         }),
         knex('users').insert({
           id: 1004,
           name: 'Richard',
           photo_url: '/images/rich.jpg',
-          username: 'Long John Silver',
+          username: 'rich',
           hash: ''
         }),
         knex('users').insert({
           id: 1005,
-          name: 'Don',
+          name: 'Don.S',
           photo_url: '/images/don.jpeg',
-          username: 'The Code Thief',
+          username: 'don.s',
           hash: ''
         }),
         knex('users').insert({
           id: 1006,
           name: 'Harrison',
           photo_url: '/images/harry.jpeg',
-          username: 'Ladybug Killer',
+          username: 'harrison',
           hash: ''
         }),
         knex('users').insert({
           id: 1007,
           name: 'Skye',
           photo_url: '/images/skye.jpeg',
-          username: 'The Sloth of the Skies',
+          username: 'skye',
           hash: ''
         }),
         knex('users').insert({
           id: 1008,
           name: 'Aaron',
           photo_url: '/images/aaron.png',
-          username: 'The Coach',
+          username: 'aaron',
           hash: ''
         }),
         knex('users').insert({
           id: 1009,
           name: 'Andrea',
           photo_url: '/images/andrea.jpeg',
-          username: 'The Bombastic Brazilian',
+          username: 'andrea',
           hash: ''
         }),
         knex('users').insert({
           id: 1010,
           name: 'Octavia',
           photo_url: '/images/oct.jpeg',
-          username: 'OctoLady',
+          username: 'octavia',
           hash: ''
         }),
         knex('users').insert({
           id: 1011,
           name: 'Daz',
           photo_url: '/images/daz.jpeg',
-          username: 'Freeform',
+          username: 'daz',
           hash: ''
         }),
         knex('users').insert({
           id: 1012,
           name: 'Annah',
           photo_url: '/images/annah.jpeg',
-          username: 'Wonder Woman',
+          username: 'annah',
           hash: ''
         }),
         knex('users').insert({
           id: 1013,
-          name: 'Don H',
+          name: 'Don.H',
           photo_url: '/images/donh.jpeg',
-          username: 'The Murricane',
+          username: 'don.h',
           hash: ''
         })
 
