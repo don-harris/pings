7cd76fae4272ed0479342093ae0d2573b00fec32
student <>
2017-10-24 11:28:36 +1300

    added select user dropdown to newPing

--- client/components/NewPing.jsx
+++ client/components/NewPing.jsx
@@ -1,5 +1,6 @@
 import {connect} from 'react-redux'
 import React from 'react'
+import data from '../../data.js'
 
 class NewPing extends React.Component {
   constructor (props) {
@@ -10,9 +11,27 @@ class NewPing extends React.Component {
   }
   render () {
     return (
-      <div><h1>New Ping will go here</h1></div>
+      <div>
+        <img/><img/><img/>
+        <form>
+          <input type="text"/><br/>
+          <select name="" className="selector">
+            <option selected disabled >Select user</option>
+            {data.users.map(user => {
+              console.log(user.name)
+              return (<option value={user.name}>{user.name}</option>)
+            })}
+          </select>
+          <br/>
+          <input type="button" value="Ping!"/>
+        </form>
+      </div>
     )
   }
+}
+
+function mapStateToProps (state) {
+
 }
 
 export default connect()(NewPing)
