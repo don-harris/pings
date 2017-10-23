7968d13ca45fe39c1526d8356cdba29b344edc31
student <>
2017-10-24 10:38:08 +1300

    added newping and profile routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import ErrorMessage from './ErrorMessage'
 import WaitIndicator from './WaitIndicator'
+import NewPing from './NewPing'
+import Profile from './Profile'
 
 const App = () => (
   <div className='app'>
     <ErrorMessage />
     <Route exact path='/' component={() => <h1>Pings</h1>} />
-    <Route exact path='/new' component={() => <h1>New Ping</h1>} />
-    <Route exact path='/profile' component={() => <h1>Profile</h1>} />
+    <Route exact path='/newping' component={NewPing} />
+    <Route exact path='/profile' component={Profile} />
     <WaitIndicator />
   </div>
 )
