d1a6ac15dfe121b3deff2f3e14a255d3beb71630
Richard Johns <richard.johns193@gmail.com>
2017-10-25 12:17:12 +1300

    kiosk mode

---
+++ client/actions/kiosk-mode.js
@@ -0,0 +1,17 @@
+
+
+export const KIOSK_ON = 'KIOSK_ON'
+
+export const kioskOn = () => {
+  return {
+    type: KIOSK_ON
+  }
+}
+
+export const KIOSK_OFF = 'KIOSK_OFF'
+
+export const kioskOff = () => {
+  return {
+    type: KIOSK_OFF
+  }
+}
