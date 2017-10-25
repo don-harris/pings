7449a0ec7a7996538d96098fa4ac7daeb4db74e1
student <>
2017-10-25 15:19:12 +1300

    I hope this is what you meant haha

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -33,10 +33,10 @@ class Profile extends React.Component {
         <img src={photo} alt="" width="200px" height="200px" className="images"/>
         <form className="form">
           <label>
-            Photo: </label> <p className="profile-photo"><input name="photo" onChange={this.handleChange} placeholder="Update Image" value={photo} /></p> <label>
-            Name:</label> <p className="profile-name"><input name="name" onChange={this.handleChange} placeholder="Your Name" value={name} /></p> <label>
-            Username:</label> <p className="profile-username"><input name="username" onChange={this.handleChange} placeholder="username" value={username} /></p> <label>
-            Password: </label><p className="profile-password" ><input name="password" onChange={this.handleChange} placeholder="password" value={password} /></p>
+            Photo:  <p className="profile-photo"><input name="photo" onChange={this.handleChange} placeholder="Update Image" value={photo} /></p> </label><label>
+            Name: <p className="profile-name"><input name="name" onChange={this.handleChange} placeholder="Your Name" value={name} /></p></label> <label>
+              Username:<p className="profile-username"><input name="username" onChange={this.handleChange} placeholder="username" value={username} /></p></label>  <label>
+            Password: <p className="profile-password" ><input name="password" onChange={this.handleChange} placeholder="password" value={password} /></p></label>
           <p className="profile-submit"><input type="submit" onClick={this.updateUser} className="button"/></p>
         </form>
       </div>
