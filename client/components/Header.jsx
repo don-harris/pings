7da5536be210eaf7396535885490666aad6137bf
memelord420 <joe@bro.com>
2017-10-25 14:35:28 +1300

    header links display if user is logged or not

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className='header'>
       <Link to='/'><h1>Pings</h1></Link>
-      <ul>
+      {!props.currentUser
+        ? <ul>
+        <li><Link to='/signin'><strong>Sign In</strong></Link></li>
+        <li><Link to='/register'><strong>Register</strong></Link></li>
+      </ul>
+        : <ul>
         <li><Link to='/new'><strong>Create new ping</strong></Link></li>
         <li><Link to='/profile'><strong>My Profile</strong></Link></li>
       </ul>
+      }
     </div>
   )
 }
 
-export default connect()(Header)
+function mapStateToProps (state) {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Header)
